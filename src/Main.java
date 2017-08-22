00168924d2d501b53b99550d1b30ea71cc33e8b4
CoffeeBrewer <cas15151@uvg.edu.gt>
2017-08-22 00:23:03 -0600

    iniciando el Main

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Created by Diego Castaneda on 21/08/2017.
+ */
+public class Main {
+
+    public static void main(String args[]){
+
+        Scanner scanner = new Scanner(System.in);
+
+        /*El usuario necesita ingresar una matriz casi cuadrada */
+        MetodosIterativos metodo = new MetodosIterativos();
+    }
+}
--- src/MetodosIterativos.java
+++ src/MetodosIterativos.java
@@ -43,4 +43,6 @@ public class MetodosIterativos {
         }
         return incognitas;
     }
+
+
 }
